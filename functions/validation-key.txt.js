c43cec9aae49da48baf7640d2dd39563662e7937
AimAze Tech Solutions <info@aimazetechsolutions.com>
2026-05-17 17:32:11 +0400

    Serve Pi validation key route

---
+++ functions/validation-key.txt.js
@@ -0,0 +1,8 @@
+export function onRequest() {
+  return new Response("bf6b0b5f6086a6c5cfe9fe85b251f95f6728c674\n", {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+      "Cache-Control": "no-store"
+    }
+  });
+}
